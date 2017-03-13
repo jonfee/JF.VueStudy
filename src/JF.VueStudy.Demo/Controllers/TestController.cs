153b0cb3c69d87af8eae8ad0ec7cd3af75e2b0b2
伍朝辉 <337883612@qq.com>
2017-03-13 15:01:55 +0800

    添加demo项目

---
+++ src/JF.VueStudy.Demo/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace JF.VueStudy.Demo.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Test()
+        {
+            return Ok("这是一个测试接口。");
+        }
+    }
+}
