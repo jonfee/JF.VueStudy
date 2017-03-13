e8f4b6cb18ecd26a932fe2d3fb9491e39e8b1358
伍朝辉 <337883612@qq.com>
2017-03-13 19:05:15 +0800

    h5 Server-Sent Events

---
+++ src/JF.VueStudy.Demo/Controllers/H5Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Web.Http;
+
+namespace JF.VueStudy.Demo.Controllers
+{
+    [RoutePrefix("api/h5/")]
+    public class H5Controller : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage sse()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            response.Content = new StringContent($"data:{DateTime.Now}\n\n", Encoding.UTF8, "text/event-stream");
+
+            return response;
+        }
+    }
+}
